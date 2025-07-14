3b82a98874e8c59ec2c1efd2fb4ec1020fb1fede
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2025-07-14 20:43:45 +0530

    Update BinaryLinkedListToInteger.cs

--- linked-list/Problems/BinaryLinkedListToInteger.cs
+++ linked-list/Problems/BinaryLinkedListToInteger.cs
@@ -1,6 +1,7 @@
 // Link - https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
 
-public class Solution 
+// Approach - 1
+public class BinaryLinkedListToInteger 
 {
     private int i = 0;
     public int GetDecimalValue(ListNode head) 
@@ -15,5 +16,19 @@ public class Solution
             result += head.val * (int)Math.Pow(2, i++);
             return result;
         }
+    }
+}
+
+// Approach - 2
+public class BinaryLinkedListToInteger 
+{
+    public int GetDecimalValue(ListNode head) 
+    {
+        var result = 0;
+        for (var node = head; node is not null; node = node.next)
+        {
+            result = result * 2 + node.val;
+        }
+        return result;
     }
 }
