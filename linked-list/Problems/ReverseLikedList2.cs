2392ed6cc1d009437c58c5d3db314b4c4a3a80fb
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2025-07-24 21:33:51 +0530

    Create ReverseLikedList2.cs

---
+++ linked-list/Problems/ReverseLikedList2.cs
@@ -0,0 +1,40 @@
+// LINK - https://leetcode.com/problems/reverse-linked-list-ii/description/
+
+public class ReverseLikedList2 
+{
+    private ListNode start;
+
+    public ListNode ReverseBetween(ListNode head, int left, int right) 
+    {
+        if (left == right) return head;
+
+        int count = 1;
+        start = head;
+
+        while (count != left)
+        {
+            start = start.next;
+            count++;
+        }
+
+        FindEndAndReplace(start.next, count + 1, left, right);
+
+        return head;
+    }
+
+    private void FindEndAndReplace(ListNode node, int current,int  left, int right)
+    {
+        if (current < right) FindEndAndReplace(node.next, current + 1, left, right);
+
+        int mid = (left + right) / 2;
+
+        if (current > mid)
+        {
+            int t = start.val;
+            start.val = node.val;
+            node.val = t;
+
+            start = start.next;
+        }
+    }
+}
