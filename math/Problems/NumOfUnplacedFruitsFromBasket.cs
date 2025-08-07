db8756752934ec735a57a6d11ba79dc5beda5b6d
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2025-08-07 11:07:02 +0530

    Update NumOfUnplacedFruitsFromBasket.cs

--- math/Problems/NumOfUnplacedFruitsFromBasket.cs
+++ math/Problems/NumOfUnplacedFruitsFromBasket.cs
@@ -3,17 +3,17 @@
 // Topic -  SQUARE ROOT DECOMPOSITION
 // We divide the baskets array into √n blocks, each of size m= √n (approximately). For each block, we maintain the maximum value in that block in an auxiliary array.
 
-public class NumOfUnplacedFruitsFromBasket
+public class Solution
 {
     public int NumOfUnplacedFruits(int[] fruits, int[] baskets)
     {
-        var m = (int)Math.Sqrt(baskets.Length);
-        var section = (baskets.Length + m - 1) / m;
-        var sectionMaxValue = new int[section];
+        var maxElementPerSection = (int)Math.Sqrt(baskets.Length);
+        var numberOfSections = (int)Math.Ceiling(baskets.Length / (double)maxElementPerSection);
+        var perSectionMaxValue = new int[numberOfSections];
         
         for (int i = 0; i < baskets.Length; i++)
         {
-            sectionMaxValue[i / m] = Math.Max(sectionMaxValue[i / m], baskets[i]);
+            perSectionMaxValue[i / maxElementPerSection] = Math.Max(perSectionMaxValue[i / maxElementPerSection], baskets[i]);
         }
 
         var unplacedFruitTypeCount = 0;
@@ -21,17 +21,17 @@ public class NumOfUnplacedFruitsFromBasket
         foreach (int fruit in fruits)
         {
             var unset = 1;
-            for (int sec = 0; sec < section; sec++)
+            for (int section = 0; section < numberOfSections; section++)
             {
-                if (sectionMaxValue[sec] < fruit)
+                if (perSectionMaxValue[section] < fruit)
                 {
                     continue;
                 }
                 int choose = 0;
-                sectionMaxValue[sec] = 0;
-                for (int i = 0; i < m; i++)
+                perSectionMaxValue[section] = 0;
+                for (int i = 0; i < maxElementPerSection; i++)
                 {
-                    int pos = sec * m + i;
+                    int pos = section * maxElementPerSection + i;
                     if (pos < baskets.Length && baskets[pos] >= fruit && choose == 0)
                     {
                         baskets[pos] = 0;
@@ -39,7 +39,7 @@ public class NumOfUnplacedFruitsFromBasket
                     }
                     if (pos < baskets.Length)
                     {
-                        sectionMaxValue[sec] = Math.Max(sectionMaxValue[sec], baskets[pos]);
+                        perSectionMaxValue[section] = Math.Max(perSectionMaxValue[section], baskets[pos]);
                     }
                 }
                 unset = 0;
