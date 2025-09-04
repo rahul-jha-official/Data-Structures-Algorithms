b757a7c69dd6baed4b81b92ae058c2b57a6431ff
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2025-09-04 22:25:54 +0530

    Create FindClosestPerson.cs

---
+++ math/Problems/FindClosestPerson.cs
@@ -0,0 +1,11 @@
+// LINK - https://leetcode.com/problems/find-closest-person/description
+
+public class FindClosestPerson 
+{
+    public int FindClosest(int x, int y, int z) 
+    {
+        var xz = Math.Abs(x - z);
+        var yz = Math.Abs(y - z);     
+        return xz == yz ? 0 : xz < yz ? 1 : 2;   
+    }
+}
