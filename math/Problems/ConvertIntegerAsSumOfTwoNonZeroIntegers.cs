820682ba36cc4b1ca7388197dac9b36a19c3a767
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2025-09-08 10:09:45 +0530

    Create ConvertIntegerAsSumOfTwoNonZeroIntegers.cs

---
+++ math/Problems/ConvertIntegerAsSumOfTwoNonZeroIntegers.cs
@@ -0,0 +1,28 @@
+// LINK - https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/description
+
+public class ConvertIntegerAsSumOfTwoNonZeroIntegers
+{
+    public int[] GetNoZeroIntegers(int n) 
+    {
+        for (int i = n - 1; i >= 1; i--)
+        {
+            if (IsNonZero(i) && IsNonZero(n - i))
+            {
+                return [n - i, i];
+            }
+        }        
+        return [];
+    }
+    private static bool IsNonZero(int n)
+    {
+        while (n > 0)
+        {
+            if ((n % 10) == 0)
+            {
+                return false;
+            }
+            n /= 10;
+        }
+        return true;
+    }
+}
