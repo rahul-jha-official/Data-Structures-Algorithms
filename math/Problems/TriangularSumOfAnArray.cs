83026d792d5f408ef767d4b6f6a0e7c1824314b4
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2025-09-30 19:45:54 +0530

    Create TriangularSumOfAnArray.cs

---
+++ math/Problems/TriangularSumOfAnArray.cs
@@ -0,0 +1,17 @@
+// LINK - https://leetcode.com/problems/find-triangular-sum-of-an-array/description/
+
+public class TriangularSumOfAnArray {
+    public int TriangularSum(int[] nums) 
+    {
+        while (nums.Length > 1)
+        {
+            var list = new int[nums.Length - 1];
+            for (var i = 1; i < nums.Length; i++)
+            {
+                list[i - 1] = (nums[i] + nums[i - 1]) % 10;
+            }
+            nums = list;
+        }        
+        return nums[0];
+    }
+}
